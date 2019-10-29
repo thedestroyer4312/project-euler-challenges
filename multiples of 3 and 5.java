a875aadbc22571ecceb8306a4288c155cba02dd4
thedestroyer4312 <57116739+thedestroyer4312@users.noreply.github.com>
2019-10-28 18:22:03 -0700

    Update multiples of 3 and 5.java

--- multiples of 3 and 5.java
+++ multiples of 3 and 5.java
@@ -1,1 +1,12 @@
-
+public class multiples3and5{
+  public static void main(String[] args){
+    System.out.println((multiples3(1000)+multiples5(1000)));
+  }
+  public static int multiples3(int input){
+    int sum=0;
+  }
+  
+  public static int multiples5(int input){
+    int sum=0;
+  }
+}

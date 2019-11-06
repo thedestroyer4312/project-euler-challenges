9a27a8c426287e4309c60ba92121addb0fb4215e
thedestroyer4312 <57116739+thedestroyer4312@users.noreply.github.com>
2019-11-06 00:04:13 -0800

    lattice paths and palindrome product WIP

--- PalindromeProduct.java
+++ PalindromeProduct.java
@@ -1,4 +1,6 @@
 /**
+ * ***WORK IN PROGRESS***
+ *Euler Problem #4
  *Finds the largest palindrome that is the product of three digit numbers
  */
 public class PalindromeProduct{
@@ -14,8 +16,15 @@ public class PalindromeProduct{
    *@return The largest palindrome product of two n-digit numbers
    */
   public static long largestPalindromeProduct(int n){
-    int max = Math.pow(10,n);
-
+    int max = (int) Math.pow(10,n);
+    long largest = -1;
+    for(int i = 0; i < max; i++){
+      for(int j = 0; j < max; j++){
+        if(isPalindrome(i * j))
+	  largest = i * j;
+      }
+    }
+    return largest;
   }
 
   /**
@@ -24,10 +33,41 @@ public class PalindromeProduct{
    *@return true if the number is a palindrome, false if it is not
    */
   public static boolean isPalindrome(long n){
-    if(n<10) //that is, n is 1 digit
+    if(n < 10) //that is, n is 1 digit
       return true;
-    else{
-
+    if(n < 100){ //that is, n is 2 digits
+      return n / 10 == n % 10;
+    }else{
+      if(firstDigit(n) != n % 10)
+        return false;
+      else
+        return isPalindrome(n / 10 - firstDigit(n / 10) * ((int) (Math.pow(10, numDigits(n / 10) - 1))));
     }
   }
+
+  /**
+   *Finds the first digit of a number n
+   *@param n A number to be evaluated
+   *@return First digit of number n
+   */
+  public static byte firstDigit(long n){
+    if(n < 10)
+     return (byte) n;
+    else
+      return firstDigit(n / 10);
+  }
+
+  /**
+   *Finds the first digit of a number n
+   *@param n A number to be evaluated
+   *@return first digit of the number n
+   */
+  public static int numDigits(long n){
+    int numDigits = 1;
+    for(int i = 10; (n / i) < 10; i *= 10){
+      numDigits++;
+    }
+    return numDigits;
+  }
 }
+
